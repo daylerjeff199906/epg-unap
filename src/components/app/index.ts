6a41e3ef86c0015420448009cd960c487d2ebe02
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-24 11:28:52 -0500

    feat: add AdminPanelLayout component

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -1,5 +1,6 @@
 import { NavBarCustom } from './NavBarCustom'
 import { UserNav } from './UserNav'
 import { MoreApps } from './MoreApps'
+import AdminPanelLayout from './admin-panel-layout'
 
-export { NavBarCustom, UserNav, MoreApps }
+export { NavBarCustom, UserNav, MoreApps, AdminPanelLayout }
