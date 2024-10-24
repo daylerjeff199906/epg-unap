94ca0caa7652e61f5fb530ecb5b6a210c439c014
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-24 11:09:54 -0500

    Refactorizar estilos de fuentes y eliminar comentarios innecesarios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,6 @@ import './globals.css'
 import { Providers } from './providers'
 import { NavBarCustom } from '@/components/app'
 
-// const geistSans = localFont({
-//   src: './fonts/GeistVF.woff',
-//   variable: '--font-geist-sans',
-//   weight: '100 900',
-// })
-// const geistMono = localFont({
-//   src: './fonts/GeistMonoVF.woff',
-//   variable: '--font-geist-mono',
-//   weight: '100 900',
-// })
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
