f456c4370f458f6640ee98157263c9fbd06c8075
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-24 12:05:50 -0500

    feat: add CollapseMenuButton component to app/index.ts and app/menu.tsx

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -2,5 +2,6 @@ import { NavBarCustom } from './NavBarCustom'
 import { UserNav } from './UserNav'
 import { MoreApps } from './MoreApps'
 import AdminPanelLayout from './admin-panel-layout'
+import { CollapseMenuButton } from './collapse-menu-button'
 
-export { NavBarCustom, UserNav, MoreApps, AdminPanelLayout }
+export { NavBarCustom, UserNav, MoreApps, AdminPanelLayout, CollapseMenuButton }
