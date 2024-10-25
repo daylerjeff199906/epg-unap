8b87dfeeb4d8069b85eecfb71cf8621d39090ffa
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-25 08:47:05 -0500

    Agregar componente MultiStepTimeline a la exportación de componentes de la aplicación

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -3,5 +3,13 @@ import { UserNav } from './user-nav'
 import { MoreApps } from './more-apps'
 import AdminPanelLayout from './admin-panel-layout'
 import { CollapseMenuButton } from './collapse-menu-button'
+import { MultiStepTimeline } from './multi-step-timeline'
 
-export { NavBarCustom, UserNav, MoreApps, AdminPanelLayout, CollapseMenuButton }
+export {
+  NavBarCustom,
+  UserNav,
+  MoreApps,
+  AdminPanelLayout,
+  CollapseMenuButton,
+  MultiStepTimeline,
+}
