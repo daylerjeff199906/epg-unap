40e93ac14a670a411381990673862644460a6379
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-25 09:38:50 -0500

    Refactorizar estilos de fuentes y ajustar el margen superior del menú

--- src/app/(admin)/admin/users/new/page.tsx
+++ src/app/(admin)/admin/users/new/page.tsx
@@ -1,17 +1,3 @@
-import { MultiStepTimeline } from '@/components/app'
-
 export default function Page() {
-  return (
-    <div className="flex flex-col lg:flex-row gap-6 container">
-      <aside>
-        <MultiStepTimeline />
-      </aside>
-      <main>
-        <h1>Formulario de creación de usuario</h1>
-        <p>
-          Este formulario le permitirá crear un nuevo usuario en el sistema.
-        </p>
-      </main>
-    </div>
-  )
+  return <div className="flex flex-col lg:flex-row gap-6 container"></div>
 }
