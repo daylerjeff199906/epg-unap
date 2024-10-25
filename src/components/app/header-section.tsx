2d5783cb17d8281fdc4d9dfa81488a5d069e2172
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-25 12:40:09 -0500

    Refactorizar componente HeaderSection para agregar botones opcionales

--- src/components/app/header-section.tsx
+++ src/components/app/header-section.tsx
@@ -6,10 +6,27 @@ interface HeaderSectionProps {
   description?: string
   showDivider?: boolean
   children?: never
+  showAddButton?: boolean
+  showRefreshButton?: boolean
+  showExportButton?: boolean
+  onAddButtonClick?: () => void
+  onRefreshButtonClick?: () => void
+  onExportButtonClick?: () => void
 }
 
 export const HeaderSection = (props: HeaderSectionProps) => {
-  const { title, description, showDivider } = props
+  const {
+    title,
+    description,
+    showDivider,
+    showAddButton = true,
+    showExportButton = true,
+    showRefreshButton = true,
+    onAddButtonClick,
+    onExportButtonClick,
+    onRefreshButtonClick,
+    children,
+  } = props
 
   return (
     <main className="p-6 rounded-lg">
@@ -23,28 +40,38 @@ export const HeaderSection = (props: HeaderSectionProps) => {
       )}
       {showDivider && <hr className="border-gray-200 my mb-4" />}
       <div className="flex space-x-2">
-        <Button
-          variant="outline"
-          className="bg-white"
-        >
-          <Plus className="mr-2 h-4 w-4" />
-          Agregar sede
-        </Button>
-        <Button
-          variant="outline"
-          className="bg-white"
-        >
-          <RefreshCw className="mr-2 h-4 w-4" />
-          Actualizar
-        </Button>
-        <Button
-          variant="outline"
-          className="bg-white"
-        >
-          <Download className="mr-2 h-4 w-4" />
-          Exportar
-        </Button>
+        {showAddButton && (
+          <Button
+            variant="outline"
+            className="bg-white"
+            onClick={onAddButtonClick}
+          >
+            <Plus className="mr-2 h-4 w-4" />
+            Agregar nuevo
+          </Button>
+        )}
+        {showRefreshButton && (
+          <Button
+            variant="outline"
+            className="bg-white"
+            onClick={onRefreshButtonClick}
+          >
+            <RefreshCw className="mr-2 h-4 w-4" />
+            Actualizar
+          </Button>
+        )}
+        {showExportButton && (
+          <Button
+            variant="outline"
+            className="bg-white"
+            onClick={onExportButtonClick}
+          >
+            <Download className="mr-2 h-4 w-4" />
+            Exportar
+          </Button>
+        )}
       </div>
+      {children && <div className="mt-4">{children}</div>}
     </main>
   )
 }
