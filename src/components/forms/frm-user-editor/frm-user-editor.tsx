50b9fa7a463a0084cdfa3bf2f2b20d559bd4d5ed
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-28 12:40:54 -0500

    Refactorizar diseño de la línea de tiempo de múltiples pasos y agregar componente de línea de tiempo en el editor de usuarios

--- src/components/forms/frm-user-editor/frm-user-editor.tsx
+++ src/components/forms/frm-user-editor/frm-user-editor.tsx
@@ -2,6 +2,15 @@ import { MultiStepTimeline } from '@/components/app'
 import { UserInfoBasic } from './sections'
 import { Button } from '@/components/ui/button'
 
+import { Step } from '@/components/app/multi-step-timeline'
+
+const steps: Step[] = [
+  {
+    id: 'user-info-basic',
+    title: 'Información básica',
+  },
+]
+
 export const FrmUserEditor = () => {
   return (
     <main className="py-4 relative h-screen max-h-[calc(100vh-104px)]">
@@ -11,7 +20,7 @@ export const FrmUserEditor = () => {
       <hr className="mt-4 border-t border-gray-300" />
       <main className="flex flex-col sm:flex-row">
         <aside className="sm:min-w-[320px] py-4">
-          <MultiStepTimeline />
+          <MultiStepTimeline steps={steps} />
         </aside>
         <div className="hidden sm:block mx-4 border-l border-gray-300 "></div>
         <section className="w-full p-4">
