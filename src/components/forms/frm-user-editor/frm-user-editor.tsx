41869b6717d279950419f56baf5dc088785c6f70
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-28 13:58:09 -0500

    Refactorizar diseño de la línea de tiempo de múltiples pasos y ajustar componente de línea de tiempo en el editor de usuarios

--- src/components/forms/frm-user-editor/frm-user-editor.tsx
+++ src/components/forms/frm-user-editor/frm-user-editor.tsx
@@ -27,6 +27,23 @@ const steps: Step[] = [
 
 export const FrmUserEditor = () => {
   const [selectedStep, setSelectedStep] = useState<string>(steps[0].id)
+  const [isStepValid, setIsStepValid] = useState<boolean>(false)
+
+  const currentStepIndex = steps.findIndex((step) => step.id === selectedStep)
+  const isLastStep = currentStepIndex === steps.length - 1
+
+  const handleNext = () => {
+    if (isStepValid) {
+      setSelectedStep(steps[currentStepIndex + 1].id)
+      setIsStepValid(false) // Reset validation for the next step
+    } else {
+      alert('Por favor, complete todos los campos requeridos.')
+    }
+  }
+
+  const validateStep = (isValid: boolean) => {
+    setIsStepValid(isValid)
+  }
 
   return (
     <main className="py-4 relative h-screen max-h-[calc(100vh-104px)]">
@@ -42,14 +59,16 @@ export const FrmUserEditor = () => {
             setSelectedStep={setSelectedStep}
           />
         </aside>
-        <div className="hidden sm:block mx-4 border-l border-gray-300 "></div>
+        <div className="hidden sm:block mx-4 border-l border-gray-300 min-h-full"></div>
         {selectedStep === 'user-info-basic' && <UserInfoBasic />}
         {selectedStep === 'user-roles-data' && <UserRolesData />}
       </main>
       <footer className="mt-4 border-t absolute left-0 right-0 bottom-0">
         <main className="flex justify-end w-full py-5">
           <Button variant="ghost">Cancelar</Button>
-          <Button>Aceptar</Button>
+          <Button onClick={handleNext}>
+            {isLastStep ? 'Finalizar' : 'Siguiente'}
+          </Button>
         </main>
       </footer>
     </main>
