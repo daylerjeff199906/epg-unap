674ce9a21df9da176ba7157edd42bc009cc11de5
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-28 10:52:48 -0500

    feat: create crud user

--- src/components/forms/frm-user-editor/frm-user-editor.tsx
+++ src/components/forms/frm-user-editor/frm-user-editor.tsx
@@ -2,11 +2,14 @@ import { UserInfoBasic } from './sections'
 
 export const FrmUserEditor = () => {
   return (
-    <main>
-      <header></header>
-      <main className="">
-        <aside></aside>
-        <section>
+    <main className="py-4">
+      <header>
+        <h1 className="font-bold text-xl">Agregar un usuario</h1>
+      </header>
+      <hr className="my-4 border-t border-gray-300" />
+      <main className="flex flex-col sm:flex-row">
+        <aside className="sm:min-w-[320px] "></aside>
+        <section className="w-full py-2">
           <UserInfoBasic />
         </section>
       </main>
--- src/components/forms/frm-user-editor/sections/user-info-basic.tsx
+++ src/components/forms/frm-user-editor/sections/user-info-basic.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const UserInfoBasic = () => {
   return (
-    <section>
+    <section id="user-info-basic">
       <header>
         <h1 className="font-bold">Configurar la información básica</h1>
         <p className="text-sm text-gray-500">
@@ -12,7 +12,10 @@ export const UserInfoBasic = () => {
         </p>
       </header>
       <hr className="my-4 border-t border-gray-300" />
-      <section className="grid grid-cols-1 gap-4 md:grid-cols-3">
+      <section
+        className="grid grid-cols-1 gap-4 md:grid-cols-3 py-4"
+        id="nro-documento"
+      >
         <div className="col-span-1 sm:col-span-1">
           <h3 className="text-sm font-bold">N° Documento</h3>
           <p className="text-xs text-gray-500">
@@ -20,9 +23,55 @@ export const UserInfoBasic = () => {
           </p>
         </div>
         <div className="col-span-1 sm:col-span-2">
-          <Input placeholder="N° Documento" />
+          <Input placeholder="N° Documento: 784542" />
         </div>
       </section>
+      <hr className="my-4 border-t border-gray-300" />
+      <section
+        className="grid grid-cols-1 gap-4 md:grid-cols-3 py-4"
+        id="name"
+      >
+        <div className="col-span-1 sm:col-span-1">
+          <h3 className="text-sm font-bold">Nombres</h3>
+          <p className="text-xs text-gray-500">
+            Ingrese los nombres de la persona
+          </p>
+        </div>
+        <div className="col-span-1 sm:col-span-2">
+          <Input placeholder="Ejemplo: Juan Jorge" />
+        </div>
+      </section>
+      <hr className="my-4 border-t border-gray-300" />
+      <section
+        className="grid grid-cols-1 gap-4 md:grid-cols-3 py-4"
+        id="primer-apellido"
+      >
+        <div className="col-span-1 sm:col-span-1">
+          <h3 className="text-sm font-bold">Prime apellido</h3>
+          <p className="text-xs text-gray-500">
+            Ingrese el primer apellido de la persona
+          </p>
+        </div>
+        <div className="col-span-1 sm:col-span-2">
+          <Input placeholder="Ejemplo: Pérez" />
+        </div>
+      </section>
+      <hr className="my-4 border-t border-gray-300" />
+      <section
+        className="grid grid-cols-1 gap-4 md:grid-cols-3 py-4"
+        id="segundo-apellido"
+      >
+        <div className="col-span-1 sm:col-span-1">
+          <h3 className="text-sm font-bold">Segundo apellido</h3>
+          <p className="text-xs text-gray-500">
+            Ingrese el segundo apellido de la persona
+          </p>
+        </div>
+        <div className="col-span-1 sm:col-span-2">
+          <Input placeholder="Ejemplo: Pérez" />
+        </div>
+      </section>
+      <hr className="my-4 border-t border-gray-300" />
     </section>
   )
 }
