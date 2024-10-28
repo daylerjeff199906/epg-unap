49af1e50d0848448b064a65728d3c48c70963489
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-28 08:40:29 -0500

    feat: Update homepage link in menu list

--- src/lib/menu-list.ts
+++ src/lib/menu-list.ts
@@ -33,7 +33,7 @@ export function getMenuList(pathname: string): Group[] {
       groupLabel: '',
       menus: [
         {
-          href: '/',
+          href: '/admin',
           label: 'Inicio',
           icon: House,
           submenus: [],
