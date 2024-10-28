0d96e67beac10c10a76418a8bae005a1a447cc6f
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-28 10:09:11 -0500

    Agregar componente HeaderSection a la página de usuarios inactivos

--- src/app/(admin)/admin/usuarios/inactivos/page.tsx
+++ src/app/(admin)/admin/usuarios/inactivos/page.tsx
@@ -1,3 +1,19 @@
+import { HeaderSection } from '@/components/app'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Usuarios sin acceso',
+  description: 'Panel de administración de EPG - UNAP',
+}
+
 export default function Page() {
-  return <div>page</div>
+  return (
+    <div>
+      <HeaderSection
+        title="Usuarios sin acceso"
+        description="Lista de usuarios dados de baja del sistema, no pueden acceder a ninguna plataforma"
+        showAddButton={false}
+      />
+    </div>
+  )
 }
