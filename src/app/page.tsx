5834b495a2d0e423b50fe171de6df7c17185ee79
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2024-10-29 09:34:16 -0500

    Remove unused TableExample component from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { TableExample } from "@/components/tables/next-ui/table-example";
-
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <TableExample /> 
     </main>
   )
 }
