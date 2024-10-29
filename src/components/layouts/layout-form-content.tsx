19477ff41e5c35c69fc5e12a9144b03f7f8166be
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-29 07:43:39 -0500

    Refactorizar diseño de la línea de tiempo de múltiples pasos y ajustar componente de línea de tiempo en el editor de usuarios

--- src/components/layouts/layout-form-content.tsx
+++ src/components/layouts/layout-form-content.tsx
@@ -1,4 +1,27 @@
+import { Button } from '../ui/button'
 
-export const LayoutFormContent = () => {
-  return <div>LayoutFormContent</div>
+interface LayoutFormContentProps {
+  children: React.ReactNode
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void
+}
+
+export const LayoutFormContent = (props: LayoutFormContentProps) => {
+  const { children, onSubmit } = props
+
+  return (
+    <>
+      <form
+        action=""
+        onSubmit={onSubmit}
+      >
+        <main>{children}</main>
+        <footer className="mt-4 border-t absolute left-0 right-0 bottom-0">
+          <main className="flex justify-end w-full py-5">
+            <Button variant="ghost">Cancelar</Button>
+            <Button type="submit">Submit</Button>
+          </main>
+        </footer>
+      </form>
+    </>
+  )
 }
