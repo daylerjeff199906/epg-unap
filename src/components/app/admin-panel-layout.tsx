ad375b404fa37517151fceeb1cef5b64b1a8b541
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-29 09:57:24 -0500

    Refactorizar manejo de dependencias: Agregar immer y zustand al proyecto

--- src/components/app/admin-panel-layout.tsx
+++ src/components/app/admin-panel-layout.tsx
@@ -1,49 +1,42 @@
-import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
-import { produce } from "immer";
+'use client'
+import { useSidebar, useStore } from '@/hooks'
+import { cn } from '@/lib/utils'
+import { SideBar } from './side-bar'
+import { BreadcrumbCustom } from './bread-crumb-custom'
 
-type SidebarSettings = { disabled: boolean; isHoverOpen: boolean };
-type SidebarStore = {
-  isOpen: boolean;
-  isHover: boolean;
-  settings: SidebarSettings;
-  toggleOpen: () => void;
-  setIsOpen: (isOpen: boolean) => void;
-  setIsHover: (isHover: boolean) => void;
-  getOpenState: () => boolean;
-  setSettings: (settings: Partial<SidebarSettings>) => void;
-};
+export default function AdminPanelLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { getOpenState, settings } = useSidebar()
+  const sidebar = useStore(useSidebar, (x) => x)
+  console.log(sidebar)
 
-export const useSidebar = create(
-  persist<SidebarStore>(
-    (set, get) => ({
-      isOpen: true,
-      isHover: false,
-      settings: { disabled: false, isHoverOpen: false },
-      toggleOpen: () => {
-        set({ isOpen: !get().isOpen });
-      },
-      setIsOpen: (isOpen: boolean) => {
-        set({ isOpen });
-      },
-      setIsHover: (isHover: boolean) => {
-        set({ isHover });
-      },
-      getOpenState: () => {
-        const state = get();
-        return state.isOpen || (state.settings.isHoverOpen && state.isHover);
-      },
-      setSettings: (settings: Partial<SidebarSettings>) => {
-        set(
-          produce((state: SidebarStore) => {
-            state.settings = { ...state.settings, ...settings };
-          })
-        );
-      }
-    }),
-    {
-      name: "sidebar",
-      storage: createJSONStorage(() => localStorage)
-    }
+  if (!sidebar) return null
+
+  return (
+    <>
+      <SideBar />
+      <main
+        className={cn(
+          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300 relative',
+          !settings.disabled && (!getOpenState() ? 'lg:ml-[90px]' : 'lg:ml-72')
+        )}
+      >
+        <section className="px-4 py-3 bg-gray-100">
+          <BreadcrumbCustom />
+        </section>
+        <main className="w-full container">{children}</main>
+      </main>
+      <footer
+        className={cn(
+          'transition-[margin-left] ease-in-out duration-300',
+          !settings.disabled && (!getOpenState() ? 'lg:ml-[90px]' : 'lg:ml-72')
+        )}
+      >
+        {/* <Footer /> */}
+      </footer>
+    </>
   )
-);
+}
