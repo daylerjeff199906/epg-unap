239b55658f6bbd0be6e26a104759356500421714
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-29 08:30:37 -0500

    Refactorizar manejo de validación en el editor de usuarios: Ajustar diseño de la línea de tiempo de múltiples pasos y componente en el editor de usuarios

--- src/components/app/admin-panel-layout.tsx
+++ src/components/app/admin-panel-layout.tsx
@@ -24,7 +24,7 @@ export default function AdminPanelLayout({
           !settings.disabled && (!getOpenState() ? 'lg:ml-[90px]' : 'lg:ml-72')
         )}
       >
-        <section className="p-4 bg-gray-100">
+        <section className="px-4 py-3 bg-gray-100">
           <BreadcrumbCustom />
         </section>
         <main className="w-full container">{children}</main>
