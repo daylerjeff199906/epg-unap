5a81e583397d7366707ae83d06ce53f77168d47d
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-30 13:41:36 -0500

    Refactorizar componente AlertCustom para mostrar iconos y ajustar tamaño de borde en la página de input y agregar sección de Input Search

--- src/app/(admin)/docs/input/page.tsx
+++ src/app/(admin)/docs/input/page.tsx
@@ -11,28 +11,39 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div>
-      <HeaderSection
-        title="Input"
-        description="Displays a form input field or a component that looks like an input field."
-        showAddButton={false}
-        showRefreshButton={false}
-        showExportButton={false}
-      />
+    <div className="flex flex-col gap-6">
+      <section>
+        <HeaderSection
+          title="Input"
+          description="Displays a form input field or a component that looks like an input field."
+          showAddButton={false}
+          showRefreshButton={false}
+          showExportButton={false}
+        />
 
-      <TabSection
-        code={`
+        <TabSection
+          code={`
 import { Input } from "@/components/ui/input"
  
 export function InputDemo() {
         return <Input type="email" placeholder="Email" />
 }
         `}
-      >
-        <section className="w-full p-6 rounded-md">
-          <Input placeholder="Prueba de input" />
-        </section>
-      </TabSection>
+        >
+          <section className="w-full p-6 rounded-md">
+            <Input placeholder="Prueba de input" />
+          </section>
+        </TabSection>
+      </section>
+      <section>
+        <HeaderSection
+          title="Input Search"
+          description="Displays a form input field or a component that looks like an input field with a search icon."
+          showAddButton={false}
+          showRefreshButton={false}
+          showExportButton={false}
+        />
+      </section>
     </div>
   )
 }
