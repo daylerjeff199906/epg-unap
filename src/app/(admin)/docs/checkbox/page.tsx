3e4e41db6e2f7d68072dcd7a181111552c82ef82
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-10-31 08:40:57 -0500

    Agregar sección de documentación para el componente Checkbox y crear el componente CheckboxCustom

--- src/app/(admin)/docs/checkbox/page.tsx
+++ src/app/(admin)/docs/checkbox/page.tsx
@@ -12,8 +12,8 @@ export default function Page() {
     <div className="flex flex-col gap-6">
       <section>
         <HeaderSection
-          title="Input Search"
-          description="Displays a form input field or a component that looks like an input field with a search icon."
+          title="Checkbox"
+          description="A control that allows the user to toggle between checked and not checked."
           showAddButton={false}
           showRefreshButton={false}
           showExportButton={false}
@@ -48,6 +48,47 @@ export function InputDemo() {
                 Accept terms and conditions
               </label>
             </div>
+          </section>
+        </TabSection>
+      </section>
+      <section>
+        <HeaderSection
+          title="Checkbox Custom"
+          description="Displays a form input field or a component that looks like an input field with a search icon."
+          showAddButton={false}
+          showRefreshButton={false}
+          showExportButton={false}
+        />
+
+        <TabSection
+          code={`
+import { InputSearch } from "@/components/ui/input"
+ 
+export function InputDemo() {
+       return (
+    <div className="flex items-center space-x-2">
+      <Checkbox id="terms" />
+      <label
+        htmlFor="terms"
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        Accept terms and conditions
+      </label>
+    </div>
+  )
+}
+        `}
+        >
+          <section className="w-full p-6 rounded-md">
+            {/* <div className="flex items-center space-x-2">
+              <Checkbox id="terms" />
+              <label
+                htmlFor="terms"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Accept terms and conditions
+              </label>
+            </div> */}
           </section>
         </TabSection>
       </section>
