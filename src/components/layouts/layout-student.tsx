c2c996a5f1593765f10051d74504f6acb91f1d79
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-05 10:38:32 -0500

    feat: fixed card program intranet

--- src/components/layouts/layout-student.tsx
+++ src/components/layouts/layout-student.tsx
@@ -15,9 +15,16 @@ export const LayoutStudent = (props: LayoutStudentProps) => {
         title={title}
         subtitle={subtitle}
       />
-      <main className="bg-primary-50">
-        <article className="container py-8 sm:p-10 lg:py-12">{children}</article>
+      <main className={`bg-primary-50`}>
+        <article className="container py-8 sm:p-10 lg:py-12 h-full">
+          {children}
+        </article>
       </main>
+      <footer>
+        <div className="container py-4 text-center text-primary-900">
+          <p>© {new Date().getFullYear()} Escuela de Postgrado UNAP</p>
+        </div>
+      </footer>
     </>
   )
 }
