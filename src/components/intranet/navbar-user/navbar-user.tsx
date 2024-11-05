20272b8444da0536684e22fd5675423793f9de5b
daylerjeff199906 <daylersan@gmail.com>
2024-11-04 21:15:51 -0500

    feat: add naviagtor

--- src/components/intranet/navbar-user/navbar-user.tsx
+++ src/components/intranet/navbar-user/navbar-user.tsx
@@ -1,9 +1,22 @@
 export const NavbarUser = () => {
   return (
-    <nav>
-      <section></section>
-      <main></main>
-      <section></section>
+    <nav className="bg-primary-900 border-b border-white">
+      <header className="container py-3">
+        <section id="navbar">
+          <div className="flex items-center gap-2">
+            <img
+              src="/brands/escudo-epg.webp"
+              alt="logo-epg"
+              className="w-8 h-12"
+            />
+            <h1 className="w-32 font-semibold text-xs text-white">
+              ESCUELA DE POSTGRADO UNAP
+            </h1>
+          </div>
+        </section>
+        <main></main>
+        <section></section>
+      </header>
     </nav>
   )
 }
