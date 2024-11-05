c093d249c51457b4af1a72631ea702e07e7f7734
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-05 11:24:50 -0500

    feat: update ProgramCard layout with new image size, badges, and action buttons

--- src/components/intranet/programs-card.tsx
+++ src/components/intranet/programs-card.tsx
@@ -1,6 +1,14 @@
+'use client'
 import { IProgram } from '@/types/intranet/IPrograms'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card'
 import { Badge } from '../ui/badge'
+import { Button } from '../ui/button'
 
 interface IProgramsCardProps {
   data: IProgram
@@ -14,21 +22,49 @@ export const ProgramCard = (props: IProgramsCardProps) => {
       <img
         src="https://blogs.ucontinental.edu.pe/wp-content/uploads/2022/09/carrera-de-administracio%CC%81n-y-gestio%CC%81n-pu%CC%81blica-800x450.jpg"
         alt={titulo}
-        className="w-72 h-48 object-cover rounded-md"
+        className="w-80 h-52 object-cover rounded-md"
       />
       <main className="w-full">
-        <CardHeader className="flex flex-row justify-between">
+        <CardHeader className="py-3 px-0">
           <CardTitle className="text-primary-900 font-bold">{titulo}</CardTitle>
-          <Badge className="bg-warning-100 text-warning-600">En curso</Badge>
+
+          <div>
+            <Badge className="bg-gray-200 text-gray-800">
+              {data.planEstudios}
+            </Badge>
+          </div>
         </CardHeader>
         <hr />
-        <CardContent className="text-gray-700 flex flex-row">
-          <main>
+        <CardContent className="text-gray-700 flex flex-row p-0">
+          <main className="p-4 grid grid-cols-1 sm:grid-cols-2 gap-x-6 gap-y-2">
             <p>Facultad: {data.facultad}</p>
+            <p>Duración: {data.duracion}</p>
+            <p>
+              Estado:{' '}
+              <span>
+                <Badge className="bg-warning-100 text-warning-600 hover:bg-warning-100">
+                  En curso
+                </Badge>
+              </span>
+            </p>
             <p>Creditos: {data.creditos}</p>
-            <p>Estado: {data.estado}</p>
           </main>
         </CardContent>
+        <CardFooter className="flex flex-row gap-2 justify-end pb-0">
+          <Button
+            size="sm"
+            variant="secondary"
+            className="border border-primary-900 text-primary-900"
+          >
+            Historial económico
+          </Button>
+          <Button
+            size="sm"
+            className="bg-primary-900 text-white"
+          >
+            Detalles académicos
+          </Button>
+        </CardFooter>
       </main>
     </Card>
   )
