fba972d708be48aa952324d9b980657d550511b8
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-06 13:01:03 -0500

    feat: agregar componente PaymentsFilters y actualizar PaymentsListPage para incluir filtros

--- src/modules/intranet/pages/payments/payments-list-page.tsx
+++ src/modules/intranet/pages/payments/payments-list-page.tsx
@@ -2,12 +2,14 @@
 import { DataTable } from '@/components/tables'
 import { paymentColumns } from './payment-columns'
 import { paymentsData } from './payments-data'
+import { PaymentsFilters } from './payments-filters'
 
 export const PaymentsListPage = () => {
   return (
     <section className="flex flex-col gap-4">
-      <section>
+      <section className="flex flex-row justify-between items-center gap-4">
         <h2 className="font-bold text-lg">Lista de pagos realizados</h2>
+        <PaymentsFilters />
       </section>
       <DataTable
         columns={paymentColumns}
