6c0ff66f8e5b5b063b4f3ffce0a44d497e6fa230
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-06 10:16:02 -0500

    feat: crear componente de página en matricula

--- src/app/(intranet)/intranet/matricula/page.tsx
+++ src/app/(intranet)/intranet/matricula/page.tsx
@@ -1,3 +1,12 @@
+import { LayoutStudent } from '@/components/layouts'
+
 export default function Page() {
-  return <div>page</div>
+  return (
+    <LayoutStudent
+      title="Matricúlate, Jhonatan Cervantes C."
+      subtitle="Inicia tu proceso de matrícula, en los programas seleccionados"
+    >
+      <article></article>
+    </LayoutStudent>
+  )
 }
