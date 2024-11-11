67a94201ae5a0619e63bdea9cdd276732a019e27
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-11 13:15:15 -0500

    feat: agregar secciones de entrada de contenido y divisores al editor de plan de estudio en etapas de matrícula

--- src/modules/academic/components/frm-enrollment-stage-editor/sections/stage-study-plan-section.tsx
+++ src/modules/academic/components/frm-enrollment-stage-editor/sections/stage-study-plan-section.tsx
@@ -1,13 +1,29 @@
-import { HeaderFormSection } from '@/components/layouts'
+import { ContentInput, HeaderFormSection } from '@/components/layouts'
+import { Divider } from '@nextui-org/react'
 import React from 'react'
 
 export const StageStudyPlanSection = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-6">
       <HeaderFormSection
         title="Selección de etapa y plan"
         description="Selecciona al periodo que pertenece a matrícula"
       />
+      <ContentInput
+        id="period"
+        label="Periodo"
+        description="Selecciona el periodo al que pertenece la etapa"
+      >
+        ASDASDAD
+      </ContentInput>
+      <Divider />
+      <ContentInput
+        id="study-plan"
+        label="Plan de estudio"
+        description="Selecciona el plan de estudios del programa, para la creación de la matrícula"
+      >
+        ASDADSASD
+      </ContentInput>
     </div>
   )
 }
