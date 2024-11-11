2a4d81a993bcfee0e526445b7ef00d5acc63c9e8
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-11 10:18:56 -0500

    feat: simplificar la página de etapas de matrículas y agregar sección de filtro

--- src/modules/academic/pages/enrollment-stages/enrollment-stages-list-page.tsx
+++ src/modules/academic/pages/enrollment-stages/enrollment-stages-list-page.tsx
@@ -1,4 +1,5 @@
 import { HeaderSection } from '@/components/app'
+import { EnrollmentStagesFilterSection } from './enrollment-stages-filter-section'
 
 export const EnrollmentStagesListPage = () => {
   return (
@@ -8,6 +9,7 @@ export const EnrollmentStagesListPage = () => {
         description='Consulta y gestiona las etapas registradas en el sistema. Usa el botón "Añadir" para crear nuevas matrículas y actualiza la información existente según sea necesario.'
         hrefAddLink="/academic/etapas-matriculas/agregar"
       />
+      <EnrollmentStagesFilterSection />
     </main>
   )
 }
