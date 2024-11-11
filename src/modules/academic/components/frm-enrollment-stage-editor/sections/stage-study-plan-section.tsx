d2d0fedd1cabac54d2a62c4c950979799da8aac6
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-11 13:19:43 -0500

    feat: agregar secciones de entrada de contenido y divisores al editor de plan de estudio en etapas de matrícula

--- src/modules/academic/components/frm-enrollment-stage-editor/sections/stage-study-plan-section.tsx
+++ src/modules/academic/components/frm-enrollment-stage-editor/sections/stage-study-plan-section.tsx
@@ -1,8 +1,30 @@
+import React from 'react'
 import { ContentInput, HeaderFormSection } from '@/components/layouts'
 import { Divider } from '@nextui-org/react'
-import React from 'react'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Check, ChevronsUpDown } from 'lucide-react'
+
+const frameworks = [] as { label: string; value: string }[]
 
 export const StageStudyPlanSection = () => {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState('')
+
   return (
     <div className="flex flex-col gap-6">
       <HeaderFormSection
@@ -14,7 +36,55 @@ export const StageStudyPlanSection = () => {
         label="Periodo"
         description="Selecciona el periodo al que pertenece la etapa"
       >
-        ASDASDAD
+        <Popover
+          open={open}
+          onOpenChange={setOpen}
+        >
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-full justify-between"
+            >
+              {value
+                ? frameworks.find((framework) => framework.value === value)
+                    ?.label
+                : 'Select framework...'}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-full p-0">
+            <Command>
+              <CommandInput placeholder="Search framework..." />
+              <CommandList>
+                <CommandEmpty>No framework found.</CommandEmpty>
+                <CommandGroup>
+                  {frameworks.map((framework) => (
+                    <CommandItem
+                      key={framework.value}
+                      value={framework.value}
+                      onSelect={(currentValue) => {
+                        setValue(currentValue === value ? '' : currentValue)
+                        setOpen(false)
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          'mr-2 h-4 w-4',
+                          value === framework.value
+                            ? 'opacity-100'
+                            : 'opacity-0'
+                        )}
+                      />
+                      {framework.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
       </ContentInput>
       <Divider />
       <ContentInput
