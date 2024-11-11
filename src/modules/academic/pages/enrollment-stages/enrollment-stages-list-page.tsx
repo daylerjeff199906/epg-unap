516034a781385070d7a66d1ed5bd882884d899b0
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-11 11:03:06 -0500

    feat: agregar tabla de datos a la página de etapas de matrículas

--- src/modules/academic/pages/enrollment-stages/enrollment-stages-list-page.tsx
+++ src/modules/academic/pages/enrollment-stages/enrollment-stages-list-page.tsx
@@ -1,15 +1,22 @@
 import { HeaderSection } from '@/components/app'
 import { EnrollmentStagesFilterSection } from './enrollment-stages-filter-section'
+import { DataTable } from '@/components/tables'
 
 export const EnrollmentStagesListPage = () => {
   return (
-    <main>
+    <>
       <HeaderSection
         title="Gestión de Etapas de matrículas"
         description='Consulta y gestiona las etapas registradas en el sistema. Usa el botón "Añadir" para crear nuevas matrículas y actualiza la información existente según sea necesario.'
         hrefAddLink="/academic/etapas-matriculas/agregar"
       />
-      <EnrollmentStagesFilterSection />
-    </main>
+      <section className="flex flex-col gap-5">
+        <EnrollmentStagesFilterSection />
+        <DataTable
+          columns={[]}
+          data={[]}
+        />
+      </section>
+    </>
   )
 }
