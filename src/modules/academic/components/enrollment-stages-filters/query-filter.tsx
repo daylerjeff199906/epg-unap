b2a7f6f34eabeba37fe3cf9a3de7383b75f36cc1
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-11 10:55:25 -0500

    feat: simplificar la página de etapas de matrículas y agregar sección de filtro

--- src/modules/academic/components/enrollment-stages-filters/query-filter.tsx
+++ src/modules/academic/components/enrollment-stages-filters/query-filter.tsx
@@ -1,3 +1,34 @@
+import { InputSearch } from '@/components/app'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+const statusOptions = [{ value: 'all', label: 'Todos' }]
+
 export const QueryFilter = () => {
-  return <div>query-filter</div>
+  return (
+    <section className="flex flex-col sm:flex-row items-center gap-3">
+      <Select defaultValue="all">
+        <SelectTrigger className="w-[180px]">
+          <label className="text-muted-foreground">Estado</label>
+          <SelectValue placeholder="Selecciona..." />
+        </SelectTrigger>
+        <SelectContent>
+          {statusOptions.map((option) => (
+            <SelectItem
+              key={option.value}
+              value={option.value}
+            >
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <InputSearch />
+    </section>
+  )
 }
--- src/modules/academic/pages/enrollment-stages/enrollment-stages-filter-section.tsx
+++ src/modules/academic/pages/enrollment-stages/enrollment-stages-filter-section.tsx
@@ -2,9 +2,11 @@ import { DatePicker, StatusFilter } from '@/modules/academic'
 
 export const EnrollmentStagesFilterSection = () => {
   return (
-    <section className="flex flex-col sm:flex-row items-center gap-3">
-      <DatePicker />
-      <StatusFilter />
-    </section>
+    <main className="flex flex-col sm:flex-row items-center gap-3 justify-between">
+      <section className="flex flex-col sm:flex-row items-center gap-3">
+        <DatePicker />
+        <StatusFilter />
+      </section>
+    </main>
   )
 }
