66c9e8745abd65d882638d90100fbb5f35eb36e3
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-11 08:59:07 -0500

    feat: add admin panel layout component

---
+++ src/app/(admin)/academic/layout.tsx
@@ -0,0 +1,28 @@
+import { AdminPanelLayout, NavBarCustom } from '@/components/app'
+import { MenuConfigApps } from '@/types/configApps'
+import type { Metadata } from 'next'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Admin Panel | EPG - UNAP',
+    default: 'Panel de administración de EPG - UNAP',
+  },
+  description: 'Panel de administración de EPG - UNAP',
+}
+
+const APP_NAME_KEY: MenuConfigApps = 'panel-admin'
+
+export default function Layout(props: LayoutProps) {
+  const { children } = props
+
+  return (
+    <>
+      <NavBarCustom app={APP_NAME_KEY} />
+      <AdminPanelLayout app={APP_NAME_KEY}>{children}</AdminPanelLayout>
+    </>
+  )
+}
