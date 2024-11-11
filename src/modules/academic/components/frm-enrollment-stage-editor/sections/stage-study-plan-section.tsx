98aa809d64b2e9e3787a1afc2107292b1320d417
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-11 13:30:27 -0500

    feat: agregar funcionalidad para seleccionar un plan de estudio en el editor de etapas de matrícula

--- src/modules/academic/components/frm-enrollment-stage-editor/sections/stage-study-plan-section.tsx
+++ src/modules/academic/components/frm-enrollment-stage-editor/sections/stage-study-plan-section.tsx
@@ -20,10 +20,14 @@ import {
 import { Check, ChevronsUpDown } from 'lucide-react'
 
 const frameworks = [] as { label: string; value: string }[]
+const studyPlans = [] as { label: string; value: string }[]
 
 export const StageStudyPlanSection = () => {
   const [open, setOpen] = React.useState(false)
+  const [openStudyPlan, setOpenStudyPlan] = React.useState(false)
+
   const [value, setValue] = React.useState('')
+  const [valueStudyPlan, setValueStudyPlan] = React.useState('')
 
   return (
     <div className="flex flex-col gap-6">
@@ -92,7 +96,58 @@ export const StageStudyPlanSection = () => {
         label="Plan de estudio"
         description="Selecciona el plan de estudios del programa, para la creación de la matrícula"
       >
-        ASDADSASD
+        <Popover
+          open={openStudyPlan}
+          onOpenChange={setOpenStudyPlan}
+        >
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={openStudyPlan}
+              className="w-full justify-between"
+            >
+              {valueStudyPlan
+                ? studyPlans.find(
+                    (studyPlan) => studyPlan.value === valueStudyPlan
+                  )?.label
+                : 'Seleccionar plan de estudio...'}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-full lg:min-w-[680px] p-0">
+            <Command>
+              <CommandInput placeholder="Buscar plan de estudio ..." />
+              <CommandList>
+                <CommandEmpty>No study plan found.</CommandEmpty>
+                <CommandGroup>
+                  {studyPlans.map((studyPlan) => (
+                    <CommandItem
+                      key={studyPlan.value}
+                      value={studyPlan.value}
+                      onSelect={(currentValue) => {
+                        setValueStudyPlan(
+                          currentValue === valueStudyPlan ? '' : currentValue
+                        )
+                        setOpenStudyPlan(false)
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          'mr-2 h-4 w-4',
+                          valueStudyPlan === studyPlan.value
+                            ? 'opacity-100'
+                            : 'opacity-0'
+                        )}
+                      />
+                      {studyPlan.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
       </ContentInput>
     </div>
   )
