7067b3091c57cfd1b1fdc29c7e0f01153a118ac8
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-12 11:04:08 -0500

    feat: eliminar componente de filtro de estado y actualizar exportaciones en filtros de etapas de inscripción

--- src/modules/academic/components/enrollment-stages-filters/index.ts
+++ src/modules/academic/components/enrollment-stages-filters/index.ts
@@ -1,6 +1,5 @@
 import { DatePicker } from './date-picker'
-import { StatusFilter } from './status-filter'
 import { QueryFilter } from './query-filter'
 import { ProgramFilter } from './program-filter'
 
-export { DatePicker, StatusFilter, QueryFilter, ProgramFilter }
+export { DatePicker, QueryFilter, ProgramFilter }
