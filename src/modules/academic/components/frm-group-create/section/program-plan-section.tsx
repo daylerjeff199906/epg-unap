7772ff707e829f1d2292621895343f9c24e5eaca
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-12 13:29:03 -0500

    feat: agregar secciones ProgramPlanSection, CourseSection y TeachersAssignedSection al componente FrmGroupCreate

--- src/modules/academic/components/frm-group-create/section/program-plan-section.tsx
+++ src/modules/academic/components/frm-group-create/section/program-plan-section.tsx
@@ -1,3 +1,12 @@
+import { HeaderFormSection } from '@/components/layouts'
+
 export const ProgramPlanSection = () => {
-  return <div>program-plan-section</div>
+  return (
+    <div className="flex flex-col gap-6">
+      <HeaderFormSection
+        title="Programa y plan de estudio"
+        description="Para la creación de los grupos es necesario que selecciones el programa y el plan de estudio a la que pertenecerá el grupo."
+      />
+    </div>
+  )
 }
