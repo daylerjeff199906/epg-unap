12c12f8d0f1ef770d27f9aa5b5bbb95c2d4feafd
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-12 13:32:06 -0500

    feat: agregar campos de entrada para seleccionar programa y plan de estudio en ProgramPlanSection

--- src/modules/academic/components/frm-group-create/section/program-plan-section.tsx
+++ src/modules/academic/components/frm-group-create/section/program-plan-section.tsx
@@ -1,4 +1,5 @@
-import { HeaderFormSection } from '@/components/layouts'
+import { ContentInput, HeaderFormSection } from '@/components/layouts'
+import { Divider } from '@nextui-org/react'
 
 export const ProgramPlanSection = () => {
   return (
@@ -7,6 +8,24 @@ export const ProgramPlanSection = () => {
         title="Programa y plan de estudio"
         description="Para la creación de los grupos es necesario que selecciones el programa y el plan de estudio a la que pertenecerá el grupo."
       />
+      <ContentInput
+        id="program"
+        label="Programa"
+        description="Selecciona el programa"
+      >
+        adsasdda
+        {/* <Input placeholder="Ejemplo: Federico villareal" /> */}
+      </ContentInput>
+      <Divider />
+      <ContentInput
+        id="plan"
+        label="Plan de estudio"
+        description="Selecciona el plan de estudio"
+        orientation="vertical"
+      >
+        asdads
+        {/* <Input placeholder="Ejemplo: Federico villareal" /> */}
+      </ContentInput>
     </div>
   )
 }
