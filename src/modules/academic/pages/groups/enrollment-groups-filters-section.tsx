32217d995aa56993edb7137652fc0b32da64b741
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-12 11:05:05 -0500

    feat: agregar componente de filtro de estado en la sección de filtros de grupos de inscripción

--- src/modules/academic/pages/groups/enrollment-groups-filters-section.tsx
+++ src/modules/academic/pages/groups/enrollment-groups-filters-section.tsx
@@ -1,9 +1,11 @@
 import { DatePickerCustom } from '@/components/app'
+import { StatusFilter } from '@/modules/academic'
 
 export const EnrollmentGroupsFiltersSection = () => {
   return (
-    <section>
+    <section className="flex flex-col sm:flex-row items-center gap-3">
       <DatePickerCustom />
+      <StatusFilter />
     </section>
   )
 }
