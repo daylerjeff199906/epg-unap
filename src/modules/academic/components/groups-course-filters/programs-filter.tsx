7bcefc596a0660e7c9f342f4fb598530e21a1131
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-12 12:51:59 -0500

    feat: actualizar título de la página de añadir grupo a mayúsculas

---
+++ src/modules/academic/components/groups-course-filters/programs-filter.tsx
@@ -0,0 +1,32 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+const statusOptions = [{ value: 'all', label: 'Todos' }]
+
+export const ProgramsFilter = () => {
+  return (
+    <div>
+      <Select defaultValue="all">
+        <SelectTrigger className="w-[220px]">
+          <label className="text-muted-foreground">Programas</label>
+          <SelectValue placeholder="Selecciona..." />
+        </SelectTrigger>
+        <SelectContent>
+          {statusOptions.map((option) => (
+            <SelectItem
+              key={option.value}
+              value={option.value}
+            >
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
