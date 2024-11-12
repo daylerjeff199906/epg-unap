a12e065c6c8ca8ceb0095c603713ae7ecc30f280
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-12 09:44:16 -0500

    feat: agregar interfaz de props y mostrar slug en la página de grupos de cursos

--- src/app/(admin)/academic/grupos/[slug]/page.tsx
+++ src/app/(admin)/academic/grupos/[slug]/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <div>P</div>
+interface IProps {
+  params: {
+    slug: string
+  }
+}
+
+export default function Page(props: IProps) {
+  const { slug } = props.params
+
+  return (
+    <div>
+      <h1>Grupos de Cursos</h1>
+      <p>Slug: {slug}</p>
+    </div>
+  )
 }
