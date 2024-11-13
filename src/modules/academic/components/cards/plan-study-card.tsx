f6ef021bd7a51e5fa230349346e25c4e885b1345
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-13 08:38:24 -0500

    feat: mejorar diseño de PlanStudyCard con contenido y pie de página

--- src/modules/academic/components/cards/plan-study-card.tsx
+++ src/modules/academic/components/cards/plan-study-card.tsx
@@ -1,11 +1,19 @@
-import { Card } from '@/components/ui/card'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
 import React from 'react'
 
 export const PlanStudyCard = () => {
   return (
     <Card>
-      <header></header>
-      <p></p>
+      <CardContent className="pt-4">
+        <header>
+          <h2 className="font-semibold">Plan de estudio</h2>
+          <h3 className="text-sm text-gray-500">Cod. 2021-1</h3>
+        </header>
+        <p></p>
+      </CardContent>
+      <CardFooter>
+        <h3 className="text-sm text-gray-600">Sede</h3>
+      </CardFooter>
     </Card>
   )
 }
