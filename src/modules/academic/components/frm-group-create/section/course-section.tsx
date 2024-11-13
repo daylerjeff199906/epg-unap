6ef1b4b7f4480b4474627e776c528a0b4b8cad7f
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-13 10:34:31 -0500

    feat: agregar funcionalidad de búsqueda y selección de cursos en CourseSection

--- src/modules/academic/components/frm-group-create/section/course-section.tsx
+++ src/modules/academic/components/frm-group-create/section/course-section.tsx
@@ -1,4 +1,17 @@
-import { HeaderFormSection } from '@/components/layouts'
+import { ContentInput, HeaderFormSection } from '@/components/layouts'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 
 export const CourseSection = () => {
   return (
@@ -7,6 +20,54 @@ export const CourseSection = () => {
         title="Curso de programa"
         description="Selecciona el curso para asignar IOS grupos, puedes facilitar tu búsqueda por ciclo"
       />
+      <section>
+        <Card className="w-full bg-gray-100">
+          <CardHeader>
+            <h1 className="text-lg font-bold">
+              Plan Seleccionado: Plan de estudio de la carrera de Ingeniería de
+              Sistemas
+            </h1>
+            <h2 className="text-gray-500 text-sm">Código del curso</h2>
+          </CardHeader>
+          <CardContent>
+            <p className="text-gray-600 text-sm">
+              El plan de estudio de la carrera de Ingeniería de Sistemas se
+              encuentra dividido en ciclos, cada ciclo contiene los cursos
+              correspondientes a ese periodo de estudio.
+            </p>
+          </CardContent>
+        </Card>
+      </section>
+      <section>
+        <ContentInput
+          id="search-course"
+          label="Buscar curso y seleccionar"
+          description="Selecciona el curso, puedes facilitar tu búsqueda por ciclo"
+          orientation="vertical"
+        >
+          <section className="flex items-center gap-4">
+            <Input placeholder="Buscar curso..." />
+            <Select>
+              <SelectTrigger className="w-[240px]">
+                <SelectValue placeholder="Seleccionar ciclo" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Ciclo</SelectLabel>
+                  <SelectItem value="1">Ciclo 1</SelectItem>
+                  <SelectItem value="2">Ciclo 2</SelectItem>
+                  <SelectItem value="3">Ciclo 3</SelectItem>
+                  <SelectItem value="4">Ciclo 4</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Button type="button">
+              <Search />
+              Buscar
+            </Button>
+          </section>
+        </ContentInput>
+      </section>
     </div>
   )
 }
