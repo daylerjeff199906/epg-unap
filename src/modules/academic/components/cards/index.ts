67b1c7e6b5a337a09af841e6433fd440991e7279
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-13 10:44:25 -0500

    feat: agregar funcionalidad de búsqueda y selección de cursos en CourseSection

--- src/modules/academic/components/cards/index.ts
+++ src/modules/academic/components/cards/index.ts
@@ -1,4 +1,5 @@
 import { TypesProgramsCard } from './type-programs-card'
 import { PlanStudyCardRadio } from './plan-study-card-radio'
+import { CoursePlanStudyCardRadio } from './course-plan-card-radio'
 
-export { TypesProgramsCard, PlanStudyCardRadio }
+export { TypesProgramsCard, PlanStudyCardRadio, CoursePlanStudyCardRadio }
