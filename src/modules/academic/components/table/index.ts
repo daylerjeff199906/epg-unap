99e4563127afc7bd57d2df652b43a1c0fb39f7b5
DanielOv1103 <danieloliveirav1103@gmail.com>
2024-11-14 12:57:55 -0500

    feat: students list page

--- src/modules/academic/components/table/index.ts
+++ src/modules/academic/components/table/index.ts
@@ -1,6 +1,6 @@
 import { groupsCourseColumn } from './groups-course-column'
 import { enrollmentStagesColumn } from './enrollment-stages-column'
 import { studentslistcolumn } from './students-list-column'
+import { StatusFilter } from './status-table-filter'
 
 export { groupsCourseColumn, enrollmentStagesColumn, studentslistcolumn, StatusFilter }
-import { StatusFilter } from './status-table-filter'
