ae6f73aa1d182e428281b14d2e5c4451fa8f4b99
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-18 10:04:22 -0500

    feat: agregar estructura básica y comentarios en la página económica

--- src/app/(admin)/economic/page.tsx
+++ src/app/(admin)/economic/page.tsx
@@ -1,3 +1,26 @@
+'use client'
+
+// import { Label } from '@/components/ui/label'
+// import { Switch } from '@/components/ui/switch'
+
+// import { useSidebar } from '@/hooks/use-sidebar'
+// import { useStore } from '@/hooks/use-store'
+
 export default function Page() {
-  return <div>page</div>
+  // const sidebar = useStore(useSidebar, (x) => x)
+  // if (!sidebar) return null
+  // const { settings, setSettings } = sidebar
+
+  return (
+    <div>
+      <div className="flex items-center space-x-2">
+        {/* <Switch
+          id="is-hover-open"
+          onCheckedChange={(x) => setSettings({ isHoverOpen: x })}
+          checked={settings.isHoverOpen}
+        />
+        <Label htmlFor="is-hover-open">Hover Open</Label> */}
+      </div>
+    </div>
+  )
 }
