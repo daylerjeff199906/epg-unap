d51107777912ae628d2abd83b1a809b1d6f30b72
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-21 08:26:19 -0500

    feat: agregar página de admisión con metadatos

---
+++ src/app/(admision)/admision/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | ADMISION | EPG - UNAP',
+    default: 'ADMISION | EPG - UNAP',
+  },
+}
+
+export default function Page() {
+  return <div>page</div>
+}
