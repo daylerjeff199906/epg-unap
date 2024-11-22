32fd41ef615a8c0742888419acb749927339ca61
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-22 08:59:01 -0500

    feat: agregar página de convocatorias con metadatos

---
+++ src/app/(admision)/admision/convocatorias/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next'
+export const metadata: Metadata = {
+  title: 'Convocatorias',
+  description:
+    'Convocatorias de admisión a la Escuela de Posgrado de la Universidad Nacional de la Amazonía Peruana.',
+}
+
+export default function page() {
+  return <div>page</div>
+}
