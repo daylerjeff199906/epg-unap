1a32c9f9d4c26d1eb65d5019740d11c9116f132c
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-11-28 09:15:36 -0500

    feat: implementar la carga de convocatorias desde la API y eliminar archivo JSON de programas

--- src/app/(admision)/admision/convocatorias/page.tsx
+++ src/app/(admision)/admision/convocatorias/page.tsx
@@ -1,5 +1,7 @@
+import { fetchCore } from '@/api/core'
 import { BannerSection } from '@/components/app'
 import { ConvocatoryListPage } from '@/modules/admision'
+import { IConvocatory } from '@/types/admision'
 import { Metadata } from 'next'
 import Image from 'next/image'
 
@@ -9,7 +11,24 @@ export const metadata: Metadata = {
     'Convocatorias de admisión a la Escuela de Posgrado de la Universidad Nacional de la Amazonía Peruana.',
 }
 
-export default function page() {
+export default async function Page() {
+  let convocatorias: IConvocatory[] = []
+
+  try {
+    // Fetch convocatorias
+    const response = await fetchCore('/api/convocatory', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    convocatorias = await response.json()
+  } catch (error) {
+    console.error('Error fetching convocatorias:', error)
+  }
+
+  console.log('convocatorias:', convocatorias)
+
   return (
     <>
       <BannerSection
@@ -27,7 +46,7 @@ export default function page() {
       />
       <main className="flex flex-col sm:flex-row gap-6 py-12 space-y-4 container">
         <aside className="w-full sm:w-1/4">
-          <ConvocatoryListPage />
+          <ConvocatoryListPage convocatorias={convocatorias} />
         </aside>
         <article className="w-full sm:w-3/4"></article>
       </main>
