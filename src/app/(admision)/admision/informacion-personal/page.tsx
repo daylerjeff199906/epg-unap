dd7148689000461d99d1d981d5374d6be7389cc5
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-12-04 11:55:57 -0500

    feat: agregar nuevo layout para la sección de información personal y simplificar la página

--- src/app/(admision)/admision/informacion-personal/page.tsx
+++ src/app/(admision)/admision/informacion-personal/page.tsx
@@ -1,6 +1,4 @@
-import { BannerSection } from '@/components/app'
 import { PersonalInfoForm } from '@/modules/admision'
-import { LayoutProfile } from '@/modules/admision/components/layouts/layout-profile'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -8,15 +6,5 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return (
-    <>
-      <BannerSection
-        title="Hola Pancracio"
-        description="Completa tus datos personales para continuar con el proceso de admisión."
-      />
-      <LayoutProfile>
-        <PersonalInfoForm />
-      </LayoutProfile>
-    </>
-  )
+  return <PersonalInfoForm />
 }
--- src/modules/admision/components/layouts/layout-profile.tsx
+++ src/modules/admision/components/layouts/layout-profile.tsx
@@ -25,10 +25,10 @@ export function LayoutProfile({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
 
   return (
-    <div className="min-h-screen bg-[#f5f8fc]">
+    <div className="bg-[#f5f8fc]">
       <main className="container py-8">
         <div className="flex flex-co sm:flex-row gap-8">
-          <nav className="bg-white rounded-lg h-fit min-w-[240px] w-80">
+          <nav className="bg-white rounded-lg h-fit min-w-[240px] w-80 sticky top-20">
             <div className="p-4 border-b">
               <h2 className="font-bold text-lg">Tus detalles</h2>
             </div>
